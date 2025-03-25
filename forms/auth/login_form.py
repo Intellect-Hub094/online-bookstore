50788ba31adf01219966f800e90c6f0bd06bffc5
Oluhle96 <21634436@dut4lifeac.onmicrosoft.com>
2025-03-25 13:15:02 +0200

    feat: Add custom email validation for login form to enforce @dut4life.ac.za domain

--- forms/auth/login_form.py
+++ forms/auth/login_form.py
@@ -1,10 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, ValidationError
+
+
+def dut4life_email(form, field):
+    if "@dut4life.ac.za" not in field.data:
+        raise ValidationError("Must be a @dut4life.ac.za email address")
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired(), Email(), dut4life_email])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Login")
